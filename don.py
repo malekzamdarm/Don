aac8d8657db77427388134d7cf48bf4c12123624
malekzamdarm <50127623+malekzamdarm@users.noreply.github.com>
2021-03-12 18:14:58 +0300

    Update and rename .gitignore to don.py

---
+++ don.py
@@ -0,0 +1,13 @@
+import  os, sys , time
+os.system("clear")
+time.sleep(2)
+print("\033[31m\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 10$ \n      Note: ID Active a DllaKam \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+while True:
+		print("\033[32m Afarin \033[0;1m:)")
+		print(50*"-")
+		time.sleep(1)
+		os.system("python2 .0.py")
+	else:
+		time.sleep(1)
+		os.system("clear")
+		print("\033[31m\n          █████▒    ▒█████     ▒██   ██▒\n        ▓██   ▒    ▒██▒  ██▒   ▒▒ █ █▒░\n        ▒████ ░   ▒██░    ██▒    ░░█░\n        ░▓█▒  ░    ▒██   ██░    ░ █ █▒\n        ░▒█░       ░ ████▓▒░   ▒██▒▒██▒\n         ▒ ░       ░ ▒░▒░▒░    ▒▒ ░ ░▓ ░\n         ░           ░ ▒ ▒░    ░░   ░▒ ░\n         ░           ░ ░ ▒      ░    ░\n                       ░ ░      ░    ░\n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n \033[0;1m     Auther: Zed\n      My lion: Dark\n      Chenal Telegram: kak_zed_1\n      Note: 10$\n      Note: ID Active a DllaKam \n\033[90;1m~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
